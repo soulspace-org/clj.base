691c8b1c9d10ee33ec20a2be490bac62a4bd6e32
Ludger Solbach <soulman@soulspace.org>
2023-03-18 13:10:57 +0100

    fixed calls to str/join, refactoring

--- test/org/soulspace/clj/test/functions.clj
+++ test/org/soulspace/clj/test/functions.clj
@@ -1,6 +1,11 @@
 (ns org.soulspace.clj.test.functions
   (:require [clojure.test :refer :all]
-          [org.soulspace.clj.function :refer :all]))
+            [org.soulspace.clj.core :refer :all]))
+
+(def my-var 0)
+(def my-atom (atom 1))
+(def my-ref (ref 2))
+(def my-agent (agent 3))
 
 (deftest not-nil?-test
   (is (not-nil? 1))
@@ -9,11 +14,6 @@
   (is (not-nil? ""))
   (is (not-nil? []))
   (is (not (not-nil? nil))))
-
-(def my-var 0)
-(def my-atom (atom 1))
-(def my-ref (ref 2))
-(def my-agent (agent 3))
 
 (deftest atom?-test
   (is (atom? my-atom))
