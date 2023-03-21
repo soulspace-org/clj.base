0c4eb127161406b3c5eadaa18f38f32bdd726c9e
Ludger Solbach <soulman@soulspace.org>
2023-03-21 19:15:23 +0100

    fixed functions test, comment updates

--- test/org/soulspace/clj/test/functions.clj
+++ test/org/soulspace/clj/test/functions.clj
@@ -1,6 +1,18 @@
+;;;;
+;;;;   Copyright (c) Ludger Solbach. All rights reserved.
+;;;;
+;;;;   The use and distribution terms for this software are covered by the
+;;;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;;;   which can be found in the file license.txt at the root of this distribution.
+;;;;   By using this software in any fashion, you are agreeing to be bound by
+;;;;   the terms of this license.
+;;;;
+;;;;   You must not remove this notice, or any other, from this software.
+;;;;
+
 (ns org.soulspace.clj.test.functions
   (:require [clojure.test :refer :all]
-          [org.soulspace.clj.function :refer :all]))
+          [org.soulspace.clj.core :refer :all]))
 
 (deftest not-nil?-test
   (is (not-nil? 1))
